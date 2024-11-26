fe03abb8ca9b5fbe49ddb78143acc235efbc0e5d
Binita <binitaacharya2004@gmail.com>
2024-11-26 11:27:48 +0545

    first commit

---
+++ android/app/src/main/kotlin/com/binita/Task3/assignment/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.binita.Task3.assignment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:assignment/view/dashboard.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Dashboard());
+  }
+}
